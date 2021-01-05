bb5c94881e6d03c5f4f1fd61e7f566797d769241
rsykes2 <rsykes1947@gmail.com>
2021-01-05 15:55:13 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { Heading, Page, Badge } from "@shopify/polaris";
+
+const Index = () => (
+  <Page
+  breadcrumbs={[{content: 'Products', url: '/products'}]}
+  title="Jar With Lock-Lid"
+  titleMetadata={<Badge status="attention">Verified</Badge>}
+  primaryAction={{content: 'Save', disabled: false}
+  secondaryActions={[{content: 'Duplicate'}, {content: 'View on your store'}]}
+  pagination={{
+    hasPrevious: true,
+    hasNext: true,
+  }}
+>
+  <p>Page content</p>
+</Page>
+);
+
+export default Index;
